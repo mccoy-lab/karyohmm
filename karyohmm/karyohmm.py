ff8622c923607fd27ba3b909add38e9b6a92d9e2
Arjun Biddanda <aabiddanda@gmail.com>
2024-01-29 11:48:10 -0500

    fix: fixed up the paternal trisomy states for metahmm

--- karyohmm/karyohmm.py
+++ karyohmm/karyohmm.py
@@ -152,9 +152,9 @@ class MetaHMM(AneuploidyHMM):
         self.p_trisomy_states = [
             (0, -1, 0, 0),
             (1, -1, 0, 0),
-            (1, -1, 1, 0),
             (0, -1, 0, 1),
             (1, -1, 0, 1),
+            (0, -1, 1, 1),
             (1, -1, 1, 1),
         ]
         if disomy:
