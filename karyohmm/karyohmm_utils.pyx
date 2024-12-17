cf16df249baa27e20c9b5e63d34a8f7e4a3274c2
Arjun Biddanda <aabiddanda@gmail.com>
2024-12-17 18:09:48 -0500

    feat: updated to use cython-lint

--- karyohmm/karyohmm_utils.pyx
+++ karyohmm/karyohmm_utils.pyx
@@ -2,18 +2,18 @@ from libc.math cimport erf, exp, expm1, log, log1p, pi, sqrt
 import numpy as np
 
 
-cdef double sqrt2 = sqrt(2.);
-cdef double sqrt2pi = sqrt(2*pi);
+cdef double sqrt2 = sqrt(2.)
+cdef double sqrt2pi = sqrt(2*pi)
 cdef double logsqrt2pi = log(1/sqrt2pi)
 
 cpdef double logsumexp(double[:] x):
     """Cython implementation of the logsumexp trick"""
-    cdef int i,n;
-    cdef double m = -1e32;
-    cdef double c = 0.0;
+    cdef int i, n
+    cdef double m = -1e32
+    cdef double c = 0.0
     n = x.size
     for i in range(n):
-        m = max(m,x[i])
+        m = max(m, x[i])
     for i in range(n):
         c += exp(x[i] - m)
     return m + log(c)
@@ -23,17 +23,17 @@ cdef double logdiffexp(double a, double b):
     return log(exp(a) - exp(b) + 1e-124)
 
 cpdef double logaddexp(double a, double b):
-    cdef double m = -1e32;
-    cdef double c = 0.0;
-    m = max(a,b)
+    cdef double m = -1e32
+    cdef double c = 0.0
+    m = max(a, b)
     c = exp(a - m) + exp(b - m)
     return m + log(c)
 
 cpdef double logmeanexp(double a, double b):
     """Apply a logmeanexp routine for two numbers."""
-    cdef double m = -1e32;
-    cdef double c = 0.0;
-    m = max(a,b)
+    cdef double m = -1e32
+    cdef double c = 0.0
+    m = max(a, b)
     c = exp(a - m) + exp(b - m)
     return m + log(c) - 2.0
 
@@ -54,7 +54,7 @@ cdef double psi(double x):
 cdef double norm_pdf(double x):
     """PDF for the normal distribution function in log-space.
 
-    NOTE: at some point we might want to generalize this to include mean-shift and stddev.
+    NOTE: generalize this to include mean-shift and stddev.
     """
     return logsqrt2pi - 0.5*(x**2)
 
@@ -64,22 +64,22 @@ cpdef double norm_logl(double x, double m, double s):
 
 cpdef double truncnorm_pdf(double x, double a, double b, double mu=0.5, double sigma=0.2):
     """Custom definition of the log of the truncated normal pdf."""
-    cdef double p, z, alpha, beta, eta;
+    cdef double p, z, alpha, beta, eta
     beta = (b - mu) / sigma
     alpha = (a - mu) / sigma
-    eta = (max(min(x,b),a) - mu) / sigma
+    eta = (max(min(x, b), a) - mu) / sigma
     z = logdiffexp(psi(beta), psi(alpha))
     p = norm_pdf(eta) - log(sigma) - z
     return p
 
 cpdef double mat_dosage(mat_hap, state):
     """Obtain the maternal dosage."""
-    cdef int k,i,l;
-    cdef double m;
+    cdef int i, j, k
+    cdef double m
     k = 0
     m = 0.
-    l = len(state)
-    for i in range(l):
+    j = len(state)
+    for i in range(j):
         k += (state[i] >= 0)
     if k == 0:
         m = -1
@@ -99,12 +99,12 @@ cpdef double mat_dosage(mat_hap, state):
     return m
 
 cpdef double pat_dosage(pat_hap, state):
-    cdef int k,i,l;
-    cdef double p;
+    cdef int i, j, k
+    cdef double p
     k = 0
     p = 0.
-    l = len(state)
-    for i in range(l):
+    j = len(state)
+    for i in range(j):
         k += (state[i] >= 0)
     if k == 0:
         p = -1
@@ -128,7 +128,7 @@ cpdef double emission_baf(double baf, double m, double p, double pi0=0.2, double
 
     NOTE: this should have some approximate error potentially?
     """
-    cdef double mu_i, x, x0, x1;
+    cdef double mu_i, x, x0, x1
     if (m == -1) & (p == -1):
         x = truncnorm_pdf(baf, 0.0, 1.0, mu=0.5, sigma=std_dev)
         return x
@@ -145,9 +145,9 @@ cpdef double emission_baf(double baf, double m, double p, double pi0=0.2, double
 
 cpdef double mix_loglik(double[:] bafs, double pi0=0.5, double theta=0.1, double std_dev=0.2):
     """Mixture log-likelihood for expected heterozygotes to estimate baf-deviation."""
-    cdef double logll = 0.0;
-    cdef int i, n;
-    cdef double ll[3];
+    cdef double logll = 0.0
+    cdef int i, n
+    cdef double ll[3]
     n = bafs.size
     for i in range(n):
         ll[0] = log(pi0/2.0) + truncnorm_pdf(bafs[i], 0.0, 1.0, mu=0.5+theta, sigma=std_dev)
@@ -156,10 +156,11 @@ cpdef double mix_loglik(double[:] bafs, double pi0=0.5, double theta=0.1, double
         logll += logsumexp(ll)
     return logll
 
+
 def lod_phase(haps1, haps2, baf, **kwargs):
     """Estimate the log-likelihood of being the phase vs. antiphase orientation for heterozygotes."""
-    cdef int i,j;
-    cdef float phase_orientation, antiphase_orientation;
+    cdef int i, j
+    cdef float phase_orientation, antiphase_orientation
     phase_orientation = 0.0
     antiphase_orientation = 0.0
     # Marginalize over all phase
@@ -186,32 +187,33 @@ def lod_phase(haps1, haps2, baf, **kwargs):
             antiphase_orientation = logaddexp(antiphase_orientation, antiphase1)
     return phase_orientation, antiphase_orientation
 
+
 def create_index_arrays(karyotypes):
     m = karyotypes.size
-    K0 = np.zeros(shape=(m,m))
-    K1 = np.zeros(shape=(m,m))
+    K0 = np.zeros(shape=(m, m))
+    K1 = np.zeros(shape=(m, m))
     ks = np.zeros(m)
     for i in range(m):
         ks[i] = np.sum(karyotypes == karyotypes[i])
         for j in range(m):
             if i != j:
                 if karyotypes[i] == karyotypes[j]:
-                    K0[i,j] = 1./ks[i]
+                    K0[i, j] = 1./ks[i]
                 else:
-                    K1[i,j] = 1./(m-ks[i])
-    return K0,K1
+                    K1[i, j] = 1./(m-ks[i])
+    return K0, K1
 
 
 cpdef transition_kernel(K0, K1, double d=1e3, double r=1e-8, double a=1e-2):
     # NOTE: should see if we need to do this in log-space for numerics ...
-    cdef int i,m;
-    cdef double rho, alpha;
+    cdef int i, m
+    cdef double rho, alpha
     m = K0.shape[0]
     rho = 1.0 - exp(-r*d)
-    alpha = 1.0  - exp(-r*a*d)
+    alpha = 1.0 - exp(-r*a*d)
     A = K0*rho + K1*alpha
     for i in range(m):
-        A[i,i] = 1. - np.sum(A[i,:])
+        A[i,i] = 1. - np.sum(A[i, :])
     return np.log(A)
 
 
