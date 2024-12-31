dfa2cad3177cd5d28c23a81ac9a05dd3c47aead1
Arjun Biddanda <aabiddanda@gmail.com>
2024-12-30 21:45:00 -0500

    fix: downgraded versioning since the changes are relatively small

--- karyohmm/__init__.py
+++ karyohmm/__init__.py
@@ -20,7 +20,7 @@ Modules exported are:
 * PGTSimVCF: module to generate synthetic PGT data based on parental data in a VCF.
 """
 
-__version__ = "0.6.0a"
+__version__ = "0.5.5a"
 
 from .io import DataReader
 from .karyohmm import (
